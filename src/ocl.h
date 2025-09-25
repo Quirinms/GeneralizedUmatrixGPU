f1dd57fac91d5522ce78df59e17f7a6c72ddc69d
Quirin <31764814+Quirinms@users.noreply.github.com>
2025-09-25 15:12:49 +0200

    Updating OCL conventions to maximize portability (OCL 1.2)

--- src/ocl.h
+++ src/ocl.h
@@ -1,10 +1,9 @@
-#define CL_TARGET_OPENCL_VERSION 210
-#define CL_USE_DEPRECATED_OPENCL_1_1_APIS
+#define CL_TARGET_OPENCL_VERSION 120
+#define CL_USE_DEPRECATED_OPENCL_1_2_APIS
 
 #ifdef __APPLE__
 #include <OpenCL/opencl.h>
 #else
 #include <CL/cl.h>
-#undef CL_VERSION_1_2
 #include <CL/cl.hpp>
 #endif
